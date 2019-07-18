13f48e8495f518a9be34adad10e91197e90800fd
tamyres <tamy.yu@hotmail.com>
2019-07-18 08:59:18 -0300

    adição da ApiRest

---
+++ Api_Server/DICs_API/DICs_API/Models/Department.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DICs_API.Models
+{
+    public class Department
+    {
+        public int Id { get; set; }
+        [Required]
+        public Users Leader { get; set; }
+        public string Name { get; set; }
+        public byte Removed { get; set; }
+    }
+
+    public class DepartmentUpload
+    {
+        public int Id { get; set; }
+        public int IdLeader { get; set; }
+        public string Name { get; set; }
+        public byte Removed { get; set; }
+    }
+
+    public class DepartmentDics
+    {
+        public Department Department { get; set; }
+        public List<DIC> Dics { get; set; }
+    }
+
+    public static class DepartmentDicsExtensions
+    {
+        public static DepartmentDics ToDepartmentDics(this IEnumerable<DIC> dics, Department dep)
+        {
+            return new DepartmentDics
+            {
+                Department = dep,
+                Dics = dics.ToList()
+            };
+        }
+    }
+
+}
---
+++ Api_Server/DICs_API/DICs_API/Models/Period.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DICs_API.Models
+{
+    public class Period
+    {
+        /// <summary>
+        /// Id do período
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Início do período
+        /// </summary>
+        [Required]
+        public DateTime StartDate { get; set; }
+        /// <summary>
+        /// Quantidade de meses
+        /// </summary>
+        [Required]
+        public int Months { get; set; }
+        /// <summary>
+        /// Término do período
+        /// </summary>
+        [Required]
+        public DateTime EndDate { get; set; }
+        /// <summary>
+        /// Período
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+        /// <summary>
+        /// Se está inativo
+        /// </summary>
+        public byte Removed { get; set; }
+    }
+}
